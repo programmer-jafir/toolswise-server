afb06d4260177b183630911e2eba8abd2b76df97
programmer-jafir <jafiranjum2020@gmail.com>
2022-05-27 00:53:42 +0600

    get tool api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -20,12 +20,20 @@ async function run(){
         await client.connect();
         const toolCollection = client.db('toolswise').collection('tools');
 
-        // all tools
+        // get all tools
         app.get('/tool', async ( req, res) =>{
             const query= {};
             const cursor = toolCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        })
+
+        // get one tool
+        app.get('/tool/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const tool = await toolCollection.findOne(query);
+            res.send(tool)
         })
     }
     finally{
